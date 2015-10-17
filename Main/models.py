9d6e30c745365a47a158630bb849272c9950dee8
inker <anton.veselyev@gmail.com>
2015-10-17 15:18:02 +0300

    added primitive models

--- Main/models.py
+++ Main/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Class(models.Model):
+    name = models.CharField(max_length=200)
+
+class Subclass(models.Model):
+    name = models.CharField(max_length=200)
+    cls = models.ForeignKey(Class)
+
+class Properties(models.Model):
+    name = models.CharField(max_length=200)
+    @property
+    def cls(self):
+        return self.subclass.cls
+    subclass = models.ForeignKey(Subclass)
+
+class Recipes(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(max_length=1000)
+    @property
+    def cls(self):
+        return self.subclass.cls
+    subclass = models.ForeignKey(Subclass)
+    properties = models.ManyToManyField(Properties)
+
+
