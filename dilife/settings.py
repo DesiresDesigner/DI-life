f8a7f55706fc09f803c3e6726dc6a4892571aeef
maxim <cuerta.espada@gmail.com>
2015-10-17 17:03:06 +0300

    Добавил миграцию, прицепил к MySQL Если хочется побаловаться:  - имя ДБ - test  - имя пользователя - root  - пароль - 1q2w3e

--- dilife/settings.py
+++ dilife/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'dilife.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',
+        'USER': 'root',
+        'PASSWORD': '1q2w3e',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
