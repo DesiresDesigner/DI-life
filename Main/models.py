91726ef07d1a82d8b4ce87a04e3df2b5d722482e
inker <anton.veselyev@gmail.com>
2015-10-17 15:37:05 +0300

    reformatted fiile

--- Main/models.py
+++ Main/models.py
@@ -1,28 +1,34 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Kingdom(models.Model):
     name = models.CharField(max_length=200)
 
+
 class Species(models.Model):
     name = models.CharField(max_length=200)
     kingdom = models.ForeignKey(Kingdom)
 
+
 class Properties(models.Model):
     name = models.CharField(max_length=200)
+
     @property
     def kingdom(self):
         return self.species.kingdom
+
     species = models.ForeignKey(Species)
+
 
 class Recipes(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(max_length=1000)
+
     @property
     def kingdom(self):
         return self.species.kingdom
+
     species = models.ForeignKey(Species)
     properties = models.ManyToManyField(Properties)
-
-
