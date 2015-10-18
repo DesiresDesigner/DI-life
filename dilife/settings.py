da13e6ffd4ac1049f757d7e84b90dc041ca41672
desiresdesigner <mialer489753>
2015-10-18 11:33:12 +0300

    Waiting result animation

--- dilife/settings.py
+++ dilife/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'dilife.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'test',
-        'USER': 'root',
-        'PASSWORD': '1q2w3e',
-        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
--- life/static/life/js/docready.js
+++ life/static/life/js/docready.js
@@ -7,11 +7,23 @@ $(document).ready(function() {
 
     $('#choose_class label').click(function(event) {
         var className = $(event.target).attr('for');
-        $('#step2').show();
+        $('#step2').slideDown(400, function(){});
     });
 
     $('#choose_subclass').change(function(event) {
         var subclassName = this.value;
-        $('#step3').show();
+        $('#step3').slideDown(400, function(){
+            /*$('#get_recipe_btn').on("click", function() {
+                alert("click");
+            });*/
+        });
     });
+
+    $('#get_recipe_btn').click(function() {
+        /*$('#waiting_submit').slideUp(400, function(){
+            $('#waiting_result').show();
+        });*/
+        $('#waiting_result_animation').show();
+    });
+
 });
