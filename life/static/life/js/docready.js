f90ae622bb625d7c93914e1a8ef6c10f8e353805
desiresdesigner <mialer489753>
2015-10-18 13:03:23 +0300

    Теперь нормально выбираются виды

--- life/static/life/js/docready.js
+++ life/static/life/js/docready.js
@@ -7,6 +7,37 @@ $(document).ready(function() {
 
     $('#choose_class label').click(function(event) {
         var className = $(event.target).attr('for');
+
+        console.log("/species/" + className);
+        $.ajax({
+            dataType: "json",
+            url: "/species/" + className,
+            success: function(result){
+                console.log(result);
+                $.each( result, function( key, val ) {
+                    /*$.each(val.stars , function(k , v ){  // The contents inside stars
+                        console.log(v);
+                    });*/
+                    //console.log(val["fields"]);
+                    var appending;
+                    if (val["fields"]["friendly_name"] == null)
+                        appending = val["fields"]["name"];
+                    else
+                        appending = val["fields"]["friendly_name"];
+                    //var append_option = "<option value='" + append + "'>" + append + "</option>";
+                    //console.log(append_option);
+
+
+                    $("#choose_subclass")
+                        .append($("<option></option>")
+                        .attr("value", appending)
+                        .text(appending))
+                        .selectpicker('refresh');
+
+                });
+            }
+        });
+
         $('#step2').slideDown(400, function(){});
     });
 
